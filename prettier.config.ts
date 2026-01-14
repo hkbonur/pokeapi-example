079d99db3cc74490075af48e4fcfc2690e9b3019
hkbonur <hkbonur@gmail.com>
2026-01-14 18:37:19 +0100

    Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+};
+
+export default config;
